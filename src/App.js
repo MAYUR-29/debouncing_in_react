02352c14fab36645c83caa275fd6e09bbd6e4adc
MAYUR-29 <mayurpatel1999@gmail.com>
2022-06-17 10:09:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {useCallback, useState} from "react";
 
 function App() {
+
+
+  const [search, setSearch] = useState();
+
+  const debounce = (func) =>{
+    let timer;
+    return function (...args){
+      const context = this;
+      if(timer) clearTimeout(timer)
+      timer = setTimeout(()=>{
+        timer = null;
+        func.apply(context,args)
+      },500)
+    }
+  }
+
+
+  // function searchbar(e){
+  //     // console.log(e.target.value);
+  //     setSearch(e.target.value);
+
+  // }
+
+  const handleChange = (e) =>{
+    const value = e.target.value;
+    // console.log(value);
+    fetch(`https://demo.dataverse.org/api/search?q=${value}`)
+    .then(res => res.json())
+    .then(json => setSearch(json.data.items));
+  }
+
+
+  const debouncedFunc = useCallback(debounce(handleChange), []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={debouncedFunc} placeholder="Search" className="search"></input>
+      {search?.length > 0 && 
+      <div className="auto">
+        {search?.map((el, i)=>
+          <div key={i}>
+            <span>{el.name}</span>
+          </div>
+        )}
+
+      </div>
+      }
     </div>
   );
 }
